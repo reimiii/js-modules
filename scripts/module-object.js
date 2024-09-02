4a598d553530f7eca7cd60f7bfcf51732bbed569
Hilmi AM <imiia75775@proton.me>
2024-09-02 13:54:11 +0700

    module object

---
+++ scripts/module-object.js
@@ -0,0 +1,14 @@
+export const firstName = "Hilmi";
+export const middleName = "Akb";
+export const lastName = "Muhrr";
+
+export function hello(){
+    console.info("Hello from Mee");
+}
+
+export class Person {
+
+    constructor() {
+        this.name = "MEe";
+    }
+}
